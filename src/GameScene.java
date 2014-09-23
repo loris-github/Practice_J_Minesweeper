98aea053a0c630e26a5886b2f5467f19fd0abcbe
loris-github <laotao200437@gmail.com>
2014-09-23 23:55:54 +0800

    carelayout
    实现了按钮转换

--- src/GameScene.java
+++ src/GameScene.java
@@ -1,4 +1,5 @@
 import java.awt.Button;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridLayout;
@@ -11,6 +12,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 class Think implements MouseListener{
+	SmallBox sb = null;
+	
+	Think(SmallBox sb){
+		this.sb = sb;
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		
 	}
@@ -21,61 +28,68 @@ class Think implements MouseListener{
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+		sb.card.next(sb);
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		Button bb = (Button)e.getSource();
-		bb.setVisible(false);
+		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 }
 
 class SmallBox extends Panel{
-	Button boxButton = null;
-	Label boxLabel = null;
-	SmallBox (String str){
+	Button tButton = new Button("T"); //top Button
+	Button bButton = new Button("B"); //bottom Button
+	CardLayout card = new CardLayout();
+	
+	SmallBox (){
 		setSize(10, 10);
 		setBackground(Color.DARK_GRAY);
-		setForeground(Color.BLUE);
-		add(boxLabel = new Label(str));
-		add(boxButton = new Button());
-		boxButton.addMouseListener(new Think());
-		boxButton.addMouseListener(new Think());
+		setForeground(Color.BLUE);		 
+		setLayout(card);		
+		add(tButton,"top");
+		add(bButton,"bottom");
+		tButton.addMouseListener(new Think(this));
+		tButton.addMouseListener(new Think(this));
 		setVisible(true);		
 	}
 }
 
 public class GameScene extends Frame {
+	public static int GS_ROWS = 4; //
+	public static int GS_COLS = 4; //
+	public static int GS_HGAP = 1; //
+	public static int GS_VGAP = 1; //
+	
 	GameScene(){
 		Collection<SmallBox> c = new ArrayList<SmallBox>();
+		
+		
 		for(int i = 0;i<16;i++){
-			SmallBox sb = new SmallBox(""+i+"");
-			//sb.addMouseListener(new Think());
+			SmallBox sb = new SmallBox();
 			c.add(sb);
 		}
-		
+		setBackground(Color.black);
 		setSize(800,600);
 		setLocation(400,800);
-		setLayout(new GridLayout(4,4));
-		
+		setLayout(new GridLayout(GS_ROWS,GS_COLS,GS_HGAP,GS_VGAP));
+
 		for(SmallBox x : c){
 			this.add(x);
 		}
 		
 		this.pack();
+		setResizable(false);
 		setVisible(true);
 	}
 	
